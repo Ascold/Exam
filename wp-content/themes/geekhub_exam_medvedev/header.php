805881100bebcdcc8c7b0db25b162a29be8b42fe
bobAscold <ascold@ex.ua>
2017-04-07 18:15:47 +0300

    test commit

--- wp-content/themes/geekhub_exam_medvedev/header.php
+++ wp-content/themes/geekhub_exam_medvedev/header.php
@@ -25,19 +25,20 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
+<!--			--><?php
+//			if ( is_front_page() && is_home() ) : ?>
+<!--				<h1 class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></h1>-->
+<!--			--><?php //else : ?>
+<!--				<p class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></p>-->
+<!--			--><?php
+//			endif;
+//
+//			$description = get_bloginfo( 'description', 'display' );
+//			if ( $description || is_customize_preview() ) : ?>
+<!--				<p class="site-description">--><?php //echo $description; /* WPCS: xss ok. */ ?><!--</p>-->
+<!--			--><?php
+//			endif; ?>
+            <?php echo get_header_image_tag(); ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
